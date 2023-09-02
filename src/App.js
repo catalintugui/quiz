05ebef22790c32a90cfccbf85dbdf33f92e3459a
Catalin Tugui <katamaxx23@gmail.com>
2023-09-02 16:40:16 +0300

    json-server, fetching data, using reducer for actions

--- src/App.js
+++ src/App.js
@@ -1,7 +1,35 @@
+import { useEffect, useReducer } from "react";
 import Header from "./Header";
 import Main from "./Main";
 
+const initialState = {
+  questions: [],
+  status: "loading", // this will change over time - loading, error, ready, active, finished
+};
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "dataReceived":
+      return { ...state, questions: action.payload, status: "ready" };
+    case "dataFailed":
+      return { ...state, status: "error" };
+    default:
+      throw new Error("Action unknown");
+  }
+}
+
 export default function App() {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  useEffect(function () {
+    fetch("http://localhost:8000/questions")
+      .then((res) => res.json())
+      .then((data) =>
+        dispatch({ type: "dataReceived", payload: data, status: "ready" })
+      )
+      .catch((err) => dispatch({ type: "dataFailed" }));
+  }, []);
+
   return (
     <div className="app">
       <Header />
